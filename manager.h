11c2366e77028271b4faa32bd176ce8b12a03243
21900279 <21900279@handong.ac.kr>
2022-04-16 18:33:43 +0900

    manager.h

---
+++ manager.h
@@ -0,0 +1,8 @@
+#include "product.h"
+void listProduct(Product *p[], int count);
+void createProduct(Product *p[], int count);
+void searchProduct(Product *p[], int count);
+void updateProduct(Product *p[], int count);
+int loadFile(Product *p[]);
+void saveFile(Product *p[], int count);
+void deleteProduct(Product *p[], int count);
